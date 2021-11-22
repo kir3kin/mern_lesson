5812a849555bda11d196c4ed15c22632eb084021
kir3kin <kir3kin@gmail.com>
2021-11-22 19:08:43 +0100

    minory update

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const http = require('http')
 const app = express()
 // const cors = require('cors')
 const path = require('path')
-
 const config = require('config')
+
 const PORT = config.get('port') || 5000
 const dbConnect = require('./db/connect.js')
 const authRouter = require('./routes/auth.router.js')
@@ -22,7 +22,12 @@ app.use('/api/link', linksRouter)
 app.use('/t', redirectRouter)
 
 if (process.env.NODE_ENV === 'production') {
-	app.use('/', express.static(path.join(__dirname, 'client', 'build', 'index.html')))
+	const buildPath = path.join(__dirname, 'client', 'build')
+	app.use('/', express.static(buildPath))
+
+	app.get('/*', function (req, res) {
+		res.sendFile(path.join(buildPath, 'index.html'));
+	})
 }
 
 const httpServer = http.createServer(app)
